f834788fdb06a1c653f7db2e8ead4a9ad2fbef1f
zhangzhiliang <zhangzhiliang@itiger.com>
2020-08-10 15:25:30 +0800

    move libs mysql utils to libs folder

--- debug.go
+++ debug.go
@@ -1,1 +1,32 @@
-package dawn_go
+package main
+
+import (
+	"github.com/zzlpeter/dawn-go/libs/redis"
+	"log"
+)
+
+
+//var c, _ = redis.Dial("tcp", ":6379")
+func readData() {
+	//c := redis.GetRedisPool("rt").Get()
+	//defer c.Close()
+	rst, err := redis.GetRedisPool("rt").GetVal("name")
+	log.Println(rst, err)
+}
+
+func readData1() {
+	rst, err := redis.GetRedisPool("local").GetVal("name")
+	log.Println(rst, err)
+}
+
+func main()  {
+
+	//fmt.Println(c, err)
+	for i := 0; i < 1; i++ {
+		readData()
+		readData1()
+	}
+	//time.Sleep(time.Second * 60)
+	//fmt.Println("done......")
+	//time.Sleep(time.Second * 30)
+}
---
+++ libs/utils/type_convert_test.go
@@ -0,0 +1,34 @@
+package utils
+
+import "testing"
+
+func TestJson2Map(t *testing.T) {
+	j := "{\"age\":12,\"name\":\"alex\"}"
+	_, err := Json2Map(j)
+	if err != nil {
+		t.Errorf("Json2Map fails")
+	}
+}
+
+func TestMap2Json(t *testing.T) {
+	m := map[string]interface{}{
+		"name": "alex",
+		"age": 24,
+	}
+	_, err := Map2Json(m)
+	if err != nil {
+		t.Errorf("Map2Json fails")
+	}
+}
+
+type User struct {
+	Name	string		`json:"name"`
+}
+
+func TestStruct2Json(t *testing.T) {
+	user := User{"alex"}
+	_, err := Struct2Json(user)
+	if err != nil {
+		t.Errorf("Struct2Json fails")
+	}
+}
